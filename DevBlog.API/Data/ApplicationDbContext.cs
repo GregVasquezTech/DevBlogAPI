2ef882de0972670ee314c298d6f0ef063885f11b
GregVasquezTech <gregfelipe98@gmail.com>
2024-04-20 09:57:53 -0400

    Added DbContext and setup db connection

---
+++ DevBlog.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DevBlog.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevBlog.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
